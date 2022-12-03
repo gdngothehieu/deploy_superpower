44c8d34d58472add6bd709a0e285c4a0b505de3c
tchong6@slb.com <ngothehieusan123@gmail.com>
2022-12-03 08:18:40 +0700

    abc

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ConfigService } from './modules/config/config.service';
-import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './modules/app/app.module';
-import { AppLogger } from './modules/app/app.logger';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as cors from 'cors';
-import { TransformInterceptor } from './modules/common/interceptors/TransformInterceptor';
-import * as express from 'express';
-import { ErrorFilter } from './modules/errors/error.filter';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { ConfigService } from "./modules/config/config.service";
+import { ValidationPipe } from "@nestjs/common";
+import { AppModule } from "./modules/app/app.module";
+import { AppLogger } from "./modules/app/app.logger";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import * as cors from "cors";
+import { TransformInterceptor } from "./modules/common/interceptors/TransformInterceptor";
+import * as express from "express";
+import { ErrorFilter } from "./modules/errors/error.filter";
 
 async function bootstrap() {
   const logger = new AppLogger();
@@ -19,20 +19,20 @@ async function bootstrap() {
     logger,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  const apiVersionPrefix: string = process.env.API_VERSION || 'api';
+  const apiVersionPrefix: string = process.env.API_VERSION || "api";
   app.setGlobalPrefix(apiVersionPrefix);
   app.useGlobalInterceptors(new TransformInterceptor());
   const options = new DocumentBuilder()
-    .setTitle('Glee2')
-    .setDescription('Glee2 API')
-    .setVersion('1.0')
-    .addTag('customTag')
+    .setTitle("Glee2")
+    .setDescription("Glee2 API")
+    .setVersion("1.0")
+    .addTag("customTag")
     .setBasePath(apiVersionPrefix)
-    .addBearerAuth()x // here is an intentional compile error. Remove the "x" and the backend should compile.
+    .addBearerAuth() // here is an intentional compile error. Remove the "x" and the backend should compile.
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
-  const config: ConfigService = app.get('ConfigService');
+  const config: ConfigService = app.get("ConfigService");
   const whitelist = config.CORS_WHITELIST;
   const corsOptions = {
     origin(origin, callback) {
@@ -41,7 +41,7 @@ async function bootstrap() {
       if (isOriginAllowed || allowAccessAnyway) {
         callback(null, true);
       } else {
-        callback(new Error('Not allowed by CORS'));
+        callback(new Error("Not allowed by CORS"));
       }
     },
   };
